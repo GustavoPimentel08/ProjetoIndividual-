b1d84f7499d6cc8665450699ddffb58bb366eb83
GustavoPimentel08 <gustavo.pcosta@sptech.school>
2025-05-20 17:11:04 -0300

    Avanço na parte contrução do index html

--- public/js/caixatexto.js
+++ public/js/caixatexto.js
@@ -32,3 +32,65 @@ const idiomas = ["Inglês", "Espanhol", "Francês"];
         mostrarPalavraComAnimacao(idiomas[index]);
       });
     }, 2500);
+
+    /* ------------------------------------------------------------------------------------- */
+
+
+
+
+
+
+
+
+
+
+    
+
+     var textos = {
+        titulo_topicos_relevantes: "Tópicos Relevantes",
+        section3: [
+          'A população do Brasil atual é de aproximadamente 212,6 milhões de habitantes',
+            'Apenas 5% do Brasil possui algum conhecimento sobre outro idioma.',
+            'E apenas 1% domina as 4 habilidades de qualquer idioma'
+        ]
+    }
+    var strings = {
+        titulo_topicos_relevantes: "Relevant Topics",
+        section3: [
+            'The country population of Brazil is 212,6 millions of citizens',
+            'bla bla bla.',
+            'And only 1% master all 4 skills of any language'
+        ]
+    }
+
+
+
+
+    var idioma = 'ptbr';
+
+    function teste() {
+        section3.innerHTML = "";
+
+        
+        if (idioma == 'ptbr') {
+            titulo_topicos_relevantes.innerHTML = textos.titulo_topicos_relevantes;
+
+            for (var i = 0; i < textos.section3.length; i++) {
+                section3.innerHTML += `
+                    <div class="box">
+                        ${textos.section3[i]}
+                    </div>
+                `
+            }
+        } else {
+            titulo_topicos_relevantes.innerHTML = strings.titulo_topicos_relevantes;
+
+            for (var i = 0; i < strings.section3.length; i++) {
+                section3.innerHTML += `
+                    <div class="box">
+                        ${strings.section3[i]}
+                    </div>
+                `
+            }
+        }
+    }
